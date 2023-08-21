2752b3c571440e27d932fed4315fde77ff12a896
Chunyang Shen <scy0208@gmail.com>
2023-08-20 18:11:05 -0700

    Upgrade

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -5,9 +5,29 @@ type UUID = string;
 export declare class Client {
     private projectId;
     private apiKey;
-    constructor(projectId: string, apiKey: string);
-    registerConfig(configName: string, config: JSONType): string;
-    storeContent(content: string, configName: string, id?: UUID, groupId?: UUID, createdBy?: string): UUID;
-    createFeedback(contentId: string, key: string, score: number, comment: string, user: string): UUID;
+    private baseUrl;
+    constructor({ projectId, apiKey, baseUrl }: {
+        projectId: string;
+        apiKey: string;
+        baseUrl?: string;
+    });
+    registerConfig({ configName, config }: {
+        configName: string;
+        config: JSONType;
+    }): string;
+    storeContent({ content, configName, id, groupId, createdBy }: {
+        content: string;
+        configName: string;
+        id?: UUID;
+        groupId?: UUID;
+        createdBy?: string;
+    }): UUID;
+    createFeedback({ contentId, key, score, comment, user }: {
+        contentId: string;
+        key: string;
+        score: number;
+        comment?: string;
+        user?: string;
+    }): UUID;
 }
 export {};
--- src/index.ts
+++ src/index.ts
@@ -7,40 +7,39 @@ export class Client {
     private projectId: string;
     private apiKey: string;
 
-    constructor(projectId: string, apiKey: string) {
+    private baseUrl: string;
+
+    constructor({ projectId, apiKey, baseUrl = "api.llmfeedback.com" }: { projectId: string; apiKey: string; baseUrl?: string; }) {
         this.projectId = projectId;
         this.apiKey = apiKey;
+        this.baseUrl = baseUrl;
     }
 
-    registerConfig(configName: string, config: JSONType): string {
-        // Here, we assume you would be storing the config somewhere, 
-        // possibly sending it to an API or saving it in a database.
-        // For simplicity, I'm just printing it out.
+    registerConfig({ configName, config }: { configName: string; config: JSONType; }): string {
         console.log(`Config ${configName} registered:`, config);
         return configName;
     }
 
-    storeContent(content: string, configName: string, id?: UUID, groupId?: UUID, createdBy?: string): UUID {
+    storeContent({ content, configName, id, groupId, createdBy }:
+        { content: string; configName: string; id?: UUID; groupId?: UUID; createdBy?: string; }): UUID {
+        
         const contentId = id || uuidv4();
-
-        // Similarly, here you would save your content data to your storage solution.
         console.log(`Content stored with ID ${contentId} and config ${configName}:`, content);
 
         if (groupId) {
             console.log(`Group ID: ${groupId}`);
         }
-
         if (createdBy) {
             console.log(`Created by: ${createdBy}`);
         }
-
+        
         return contentId;
     }
 
-    createFeedback(contentId: string, key: string, score: number, comment: string, user: string): UUID {
+    createFeedback({ contentId, key, score, comment, user }:
+        { contentId: string; key: string; score: number; comment?: string; user?: string; }): UUID {
+        
         const feedbackId = uuidv4();
-
-        // Store the feedback. Again, for simplicity, just printing it out.
         console.log(`Feedback for content ID ${contentId}:`);
         console.log(`Key: ${key}`);
         console.log(`Score: ${score}`);
